dd8128481874f56f1349716431405bd1315f71c2
jenjensha <14373089+jenjensha@users.noreply.github.com>
2018-04-25 23:13:47 +0200

    try formcarry

--- static/js/agency.js
+++ static/js/agency.js
@@ -47,19 +47,19 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 // Async contact form
-// $('form[id=contactForm]').submit(function(){
-//   $.post("https://vigilant-kilby-9705b8.netlify.com", $(this).serialize(), function(data, textStatus, jqXHR){
-//     $('form[id=contactForm] #success').hide();
-//     $('form[id=contactForm] #error').hide();
-//     if (jqXHR.status == 200) {
-//       $('form[id=contactForm] #success').show();
-//     }}, 'json').fail(function(){
-//       $('form[id=contactForm] #success').hide();
-//       $('form[id=contactForm] #error').hide();
-//       $('form[id=contactForm] #error').show();
-//   });
-//   return false;
-// });
+$('form[id=contactForm]').submit(function(){
+  $.post($(this).attr('action'), $(this).serialize(), function(data, textStatus, jqXHR){
+    $('form[id=contactForm] #success').hide();
+    $('form[id=contactForm] #error').hide();
+    if (jqXHR.status == 200) {
+      $('form[id=contactForm] #success').show();
+    }}, 'json').fail(function(){
+      $('form[id=contactForm] #success').hide();
+      $('form[id=contactForm] #error').hide();
+      $('form[id=contactForm] #error').show();
+  });
+  return false;
+});
 
 // Contact form validation
 $.validate({
